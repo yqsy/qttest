87d353b40ab2f668548635de8288a5434014b1a5
yqsy <yqsy021@126.com>
2017-09-12 07:58:16 +0800

    总算搞明白了

--- timer.py
+++ timer.py
@@ -3,13 +3,22 @@ from PyQt5.QtCore import QObject, QTimer, pyqtSlot
 from PyQt5.QtWidgets import QApplication
 
 
+class MyTime(QTimer):
+    def __init__(self, *args, **kwargs):
+        super(MyTime, self).__init__(*args, **kwargs)
+
+    def __del__(self):
+        print('del')
+
+
 class Worker(QObject):
     def __init__(self):
         super(Worker, self).__init__()
 
-        timer = QTimer(self)
+        # 如果不设置父对象就会被销毁
+        timer = MyTime(self)
         timer.timeout.connect(self.xxx)
-        timer.start(1000)
+        timer.start(300)
 
     # 这个装饰函数一定要用哦
     @pyqtSlot()
@@ -18,8 +27,6 @@ class Worker(QObject):
 
 
 def main():
-
-
     app = QApplication(sys.argv)
 
     worker = Worker()

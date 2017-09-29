fd9b4fcefa56444ba292b7686b90a307d3fe7ac7
Guochun.Gong <yqsy021@126.com>
2017-09-29 13:34:12 +0800

    qtimer_test

---
+++ qtimer_test.py
@@ -0,0 +1,35 @@
+import threading
+
+import time
+
+import sys
+from PyQt5.QtCore import QThreadPool, QRunnable, QTimer, QObject
+from PyQt5.QtWidgets import QApplication
+
+
+class TimerTest(QObject):
+    def __init__(self):
+        super(TimerTest, self).__init__()
+        self._init_time()
+
+    def _init_time(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self._timer_out)
+        self.timer.start(1000)
+
+    def _timer_out(self):
+        print('timer out, thread id: ', threading.get_ident())
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    print('main thread id: ', threading.get_ident())
+
+    timer_test = TimerTest()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()

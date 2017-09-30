af003c59c2f72b4f062d9660b082b458a3f8feff
yqsy <yqsy021@126.com>
2017-09-30 14:18:47 +0800

    针对几个时间做整理

--- socket_test2.py
+++ socket_test2.py
@@ -2,10 +2,10 @@
 测试触发信号
 *void	connected()
 *void	disconnected()
-void	error(QAbstractSocket::SocketError socketError)
+*void	error(QAbstractSocket::SocketError socketError)
 void	hostFound()
 void	proxyAuthenticationRequired(const QNetworkProxy &proxy, QAuthenticator *authenticator)
-void	stateChanged(QAbstractSocket::SocketState socketState)
+*void	stateChanged(QAbstractSocket::SocketState socketState)
 
 void	aboutToClose()
 *void	bytesWritten(qint64 bytes)
@@ -19,10 +19,27 @@ void	objectNameChanged(const QString &objectName)
 """
 import sys
 import traceback
+import logging
 
 from PyQt5.QtCore import QObject, pyqtSlot
-from PyQt5.QtNetwork import QTcpSocket
+from PyQt5.QtNetwork import QTcpSocket, QAbstractSocket
 from PyQt5.QtWidgets import QApplication
+
+FORMAT = '%(asctime)s %(thread)d %(levelname)s %(filename)s:%(lineno)d %(message)s'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[]
+)
+
+logger = logging.getLogger(__name__)
+
+
+class QDebug(object):
+    pass
+
+
+IP = '202.5.19.132'
 
 
 class SocketTest(QObject):
@@ -33,39 +50,89 @@ class SocketTest(QObject):
         self.socket = QTcpSocket(self)
 
         def connected():
-            print('connected , local port:{}'.format(self.socket.localPort()))
-            self.socket.write('fuck u'.encode(encoding='utf8'))
+            logger.info('connected , local port:{}'.format(self.socket.localPort()))
+            # self.socket.write('fuck u'.encode(encoding='utf8'))
 
         def disconnect():
-            print('disconnected')
+            logger.info('disconnected')
+
+            self.socket.connectToHost(IP, 443)
+
+        @pyqtSlot(QAbstractSocket.SocketError)
+        def error(err):
+            dict = {
+                0: 'QAbstractSocket::ConnectionRefusedError',
+                1: 'QAbstractSocket::RemoteHostClosedError',
+                2: 'QAbstractSocket::HostNotFoundError',
+                3: 'QAbstractSocket::SocketAccessError',
+                4: 'QAbstractSocket::SocketResourceError',
+                5: 'QAbstractSocket::SocketTimeoutError',
+                6: 'QAbstractSocket::DatagramTooLargeError',
+                7: 'QAbstractSocket::NetworkError',
+                8: 'QAbstractSocket::AddressInUseError',
+                9: 'QAbstractSocket::SocketAddressNotAvailableError',
+                10: 'QAbstractSocket::UnsupportedSocketOperationError',
+                12: 'QAbstractSocket::ProxyAuthenticationRequiredError',
+                13: 'QAbstractSocket::SslHandshakeFailedError',
+                11: 'QAbstractSocket::UnfinishedSocketOperationError',
+                14: 'QAbstractSocket::ProxyConnectionRefusedError',
+                15: 'QAbstractSocket::ProxyConnectionClosedError',
+                16: 'QAbstractSocket::ProxyConnectionTimeoutError',
+                17: 'QAbstractSocket::ProxyNotFoundError',
+                18: 'QAbstractSocket::ProxyProtocolError',
+                19: 'QAbstractSocket::OperationError',
+                20: 'QAbstractSocket::SslInternalError',
+                21: 'QAbstractSocket::SslInvalidUserDataError',
+                22: 'QAbstractSocket::TemporaryError',
+                -1: 'QAbstractSocket::UnknownSocketError'
+            }
+
+            logger.info(dict[err])
+
+        @pyqtSlot(QAbstractSocket.SocketState)
+        def _state_changed(state):
+            # http://doc.qt.io/qt-5/qabstractsocket.html#SocketState-enum
+            dict = {
+                0: 'QAbstractSocket::UnconnectedState',
+                1: 'QAbstractSocket::HostLookupState',
+                2: 'QAbstractSocket::ConnectingState',
+                3: 'QAbstractSocket::ConnectedState',
+                4: 'QAbstractSocket::BoundState',
+                6: 'QAbstractSocket::ClosingState',
+                5: 'QAbstractSocket::ListeningState'
+            }
+
+            logger.info(dict[state])
 
         def ready_ready():
-            print('ready read', end=': ')
+            logger.info('ready read', end=': ')
 
-            print(self.socket.readAll())
+            logger.info(self.socket.readAll())
 
             self.socket.close()
 
-        def reconnect():
-            self.socket.connectToHost('202.5.19.132', 22)
-
         @pyqtSlot(int)
         def bytes_written(bytes):
-            print('{} bytes send'.format(bytes))
+            logger.info('{} bytes send'.format(bytes))
 
         self.socket.connected.connect(connected)
         self.socket.disconnected.connect(disconnect)
-        self.socket.readyRead.connect(ready_ready)
-        self.socket.bytesWritten.connect(bytes_written)
-
-        self.socket.disconnected.connect(reconnect)
-
-        self.socket.connectToHost('202.5.19.132', 22)
+        self.socket.stateChanged.connect(_state_changed)
+        self.socket.error.connect(error)
+        # self.socket.readyRead.connect(ready_ready)
+        # self.socket.bytesWritten.connect(bytes_written)
+        self.socket.connectToHost(IP, 443)
 
 
 def main():
     sys.stdout.flush()
     sys.excepthook = traceback.print_exception
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(logging.Formatter(FORMAT))
+    logger.addHandler(console)
+
     app = QApplication(sys.argv)
     socket = SocketTest()
     socket.init_socket()
